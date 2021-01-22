fd06cff9d8c27fddda85d2521a4ad7a99743de6f
Alex Starichenko <68742649+Glasgalas@users.noreply.github.com>
2021-01-22 23:21:55 +0200

    сокращение повторяющегося кода

--- src/js/theme.js
+++ src/js/theme.js
@@ -10,9 +10,19 @@ const bodyRef = document.querySelector('body');
 // переменная для выбора переключателя
 const toggleRef = document.querySelector('#theme-switch-toggle');
 
+// функции на добавление и удаление класса светлой темы для сокращения повторения кода
+function removeClassLight() {
+  bodyRef.classList.remove(Theme.LIGHT);
+}
+
+function addClassLight() {
+  bodyRef.classList.add(Theme.LIGHT);
+}
+
 // установка темы по умолчанию
 function setDefaultTheme() {
-  bodyRef.classList.add(Theme.LIGHT);
+  addClassLight();
+  // bodyRef.classList.add(Theme.LIGHT);
   const localSavedTheme = localStorage.getItem('theme');
   // если вернет null то ничего не произойдет и тема применится светлая по умолчанию, если вернет значение ранее сохраненной темы - она установится на body
   if (localSavedTheme) {
@@ -23,12 +33,14 @@ function setDefaultTheme() {
 // смена темы по переключению
 function changeTheme() {
   if (toggleRef.checked) {
-    bodyRef.classList.remove(Theme.LIGHT);
+    removeClassLight();
+    // bodyRef.classList.remove(Theme.LIGHT);
     bodyRef.classList.add(Theme.DARK);
     localStorage.setItem('theme', JSON.stringify(Theme.DARK));
   } else {
     bodyRef.classList.remove(Theme.DARK);
-    bodyRef.classList.add(Theme.LIGHT);
+    addClassLight();
+    // bodyRef.classList.add(Theme.LIGHT);
     localStorage.setItem('theme', JSON.stringify(Theme.LIGHT));
   }
 }
@@ -37,7 +49,8 @@ function changeTheme() {
 function checkTheme() {
   if (bodyRef.classList.contains(Theme.DARK)) {
     toggleRef.checked = true;
-    bodyRef.classList.remove(Theme.LIGHT);
+    removeClassLight();
+    // bodyRef.classList.remove(Theme.LIGHT);
   }
 }
 // слушатель событий на переключатель тем  и вызов функции на смену темы
